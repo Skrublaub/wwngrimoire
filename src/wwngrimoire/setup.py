ec7981fb7d4458187c6fe83c76fd704d7e8b5370
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-08-06 20:30:07 -0500

    Finalize setup.py

--- src/wwngrimoire/setup.py
+++ src/wwngrimoire/setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='wwngrimoire',
-    version='0.0.1',
+    name="wwngrimoire",
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'wg = wwngrimoire.__init__:enter'
-        ]
-    }
+    entry_points={"console_scripts": ["wg = wwngrimoire.__init__:enter"]},
 )
