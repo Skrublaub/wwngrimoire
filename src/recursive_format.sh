79ff70c3e722987213d0645880a45402cb7c2c79
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-08-14 14:04:40 -0500

    Wrap -regex parameters in quotes

--- src/recursive_format.sh
+++ src/recursive_format.sh
@@ -7,8 +7,8 @@ conda activate wwn
 # finds path to all python files recursively and
 # runs black on them individually
 # {} + won't work if over 50 files found
-find . -regex .*.py -exec black {} \;
-find . -regex .*.html -exec npx prettier -w {} \;
-find . -regex .*.js -exec npx prettier -w {} \;
+find . -regex ".*.py" -exec black {} \;
+find . -regex ".*.html" -exec npx prettier -w {} \;
+find . -regex ".*.js" -exec npx prettier -w {} \;
 
 # I don't want to use pre-commit
