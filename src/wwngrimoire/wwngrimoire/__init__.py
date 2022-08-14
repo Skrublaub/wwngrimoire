8fb75834a8e6d57ac521b23a7a7eb289475889f2
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-08-14 14:01:42 -0500

    add logging and "/", "/home" paths

--- src/wwngrimoire/wwngrimoire/__init__.py
+++ src/wwngrimoire/wwngrimoire/__init__.py
@@ -1,5 +1,7 @@
+import logging
+
 from typer import Typer, Option
-from flask import Flask
+from flask import Flask, url_for, redirect
 
 from wwngrimoire.utils.log import initiate_logger
 from wwngrimoire.utils.spell_utils import generate_json as utils_generate_json
@@ -11,7 +13,8 @@ cli: Typer = Typer()
 
 app.register_blueprint(no_js_bp)
 
-# initiate_logger()
+# initiate_logger()  # makes flask double print
+logger = logging.getLogger(__name__)
 
 
 @cli.command(help="start the wwngrimoire server")
@@ -26,6 +29,13 @@ def start(
 @cli.command(help="generate spell(s) json")
 def generate_json() -> None:
     utils_generate_json()
+
+
+@app.route("/")
+@app.route("/home")
+def home_page():
+    no_js_url: url_for = url_for('no_js.no_js_home_page')
+    return redirect(no_js_url)
 
 
 def enter() -> None:
