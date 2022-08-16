af1d9a4d81544618bcc150a8034aebaf6ebf754f
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-08-15 20:51:34 -0500

    add more routes

--- src/wwngrimoire/wwngrimoire/pages/no_js.py
+++ src/wwngrimoire/wwngrimoire/pages/no_js.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import redirect, render_template, Blueprint
+from flask import render_template, Blueprint
 
 no_js_bp = Blueprint("no_js", __name__)  # no javascript blueprint
 logger = logging.getLogger(__name__)
@@ -9,3 +9,13 @@ logger = logging.getLogger(__name__)
 @no_js_bp.route("/nojs")
 def no_js_home_page():
     return render_template("home.html")
+
+
+@no_js_bp.route("/about")
+def about_page():
+    return render_template("about.html")
+
+
+@no_js_bp.route("/nojs/search/<query>")
+def no_js_search(query):
+    pass
